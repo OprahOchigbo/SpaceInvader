b91e7c68e352fb87d113f37d3a932833a3ee7f00
ItsMollyHere <sofhil2007@gmail.com>
2024-10-25 13:23:29 +0200

    Not finished throwing code
    code not working atm

--- My project (6)/Assets/Code/Player.cs
+++ My project (6)/Assets/Code/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 
@@ -7,45 +8,80 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
+    private IEnumerator ThrowDessert;
     public Laser laserPrefab;
     public Animator Animator;
     Laser laser;
+    Rigidbody2D rbody;
     float speed = 5f;
-    float horizontalMove = 0f; 
+    float horizontalMove = 0f;
+    bool Throwing = false;
+    bool Moving = true;
+
+    private void Start()
+    {
+
+        rbody = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        Vector3 position = transform.position;
 
-        position.x += horizontalMove * Time.deltaTime;
 
-        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
-
-        if (horizontalMove < 0)
+        if (Input.GetButtonDown("Jump") && !Throwing)
         {
-            transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
-        }
-        else if (horizontalMove > 0)
-        {
-            transform.localScale = new Vector3(-1.3f, 1.3f, 1.3f);
-        }
+            //StartCoroutine(ThrowDessert());
 
-        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
-        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
-
-        position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
-
-        transform.position = position;
-        
-
-        if (Input.GetKeyDown(KeyCode.Space) && laser == null)
-        {
             laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            
         }
+
+        /*IEnumerator ThrowDessert()
+        {
+
+            Throwing = true;
+            Animator.SetBool("Throw", true);
+            Moving = false;
+        }*/
+
+        if (Moving)
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+            Animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+            Vector3 position = transform.position;
+            position.x += horizontalMove * Time.deltaTime;
+            transform.position = position;
+
+
+            if (horizontalMove < 0)        //move left
+            {
+                transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+            }
+            else if (horizontalMove > 0)    //move right
+            {
+                transform.localScale = new Vector3(-1.3f, 1.3f, 1.3f);
+            }
+
+            Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+            Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+            position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
+ 
+     
+        }
+
     }
+    public void FinishThrow()
+    {
+        Throwing = false;
+        Moving = true;
+        Animator.SetBool("Throwing", false);
+    }
+
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
