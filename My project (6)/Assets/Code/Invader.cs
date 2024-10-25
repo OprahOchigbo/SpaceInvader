3a77b1969623d76d84aee5da52eb57323bf0ec05
ChingChongggg <svante.gustavssonblomberg@elev.ga.lbs.se>
2024-10-25 13:19:39 +0200

    HitMarker

--- My project (6)/Assets/Code/Invader.cs
+++ My project (6)/Assets/Code/Invader.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Build.Content;
 using UnityEngine;
-
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
@@ -13,27 +10,34 @@ public class Invader : MonoBehaviour
     public Sprite[] animationSprites = new Sprite[2];
     public float animationTime;
 
-    SpriteRenderer spRend;
-    int animationFrame;
-    // Start is called before the first frame update
+    public Color hitColor = Color.red;  // Color to apply when the invader gets hit
+    public float hitColorDuration = 0.5f; // Duration to show the hit color
+
+    private SpriteRenderer spRend;
+    private int animationFrame;
+    private bool isHit = false;
+    private Color originalColor; // Store the original color
 
     private void Awake()
     {
         spRend = GetComponent<SpriteRenderer>();
         spRend.sprite = animationSprites[0];
+
+        // Store the original color
+        originalColor = spRend.color;
     }
 
     void Start()
     {
-        //Anropar AnimateSprite med ett visst tidsintervall
-        InvokeRepeating( nameof(AnimateSprite) , animationTime, animationTime);
+        // Start the sprite animation
+        InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
     }
 
-    //pandlar mellan olika sprited för att skapa en animation
+    // Animates between different sprites for the invader
     private void AnimateSprite()
     {
         animationFrame++;
-        if(animationFrame >= animationSprites.Length)
+        if (animationFrame >= animationSprites.Length)
         {
             animationFrame = 0;
         }
@@ -42,14 +46,44 @@ public class Invader : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Laser"))
+        // If hit by a laser
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Laser"))
         {
+            if (isHit) return; // If already hit, do nothing
+
+            // Destroy the laser immediately
+            Destroy(collision.gameObject);
+
+            // Mark the invader as hit
+            isHit = true;
+
+            // Notify GameManager
             GameManager.Instance.OnInvaderKilled(this);
+
+            // Change the color to indicate hit
+            StartCoroutine(FlashColor(hitColor, hitColorDuration));
+
+            // Start the destruction process with a delay
+            StartCoroutine(DestroyAfterDelay(0.5f));
         }
-        else if(collision.gameObject.layer == LayerMask.NameToLayer("Boundary")) //nått nedre kanten
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Boundary"))
         {
             GameManager.Instance.OnBoundaryReached();
         }
     }
 
+    // Coroutine to flash color when hit
+    private IEnumerator FlashColor(Color newColor, float duration)
+    {
+        spRend.color = newColor; // Change to the hit color
+        yield return new WaitForSeconds(duration);
+        spRend.color = originalColor; // Revert to the original color
+    }
+
+    // Coroutine to destroy the invader after a delay
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);  // Destroy the invader after the delay
+    }
 }
